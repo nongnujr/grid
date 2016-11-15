756a6225141b0dac7b94761fb4d9a91ab3cddac9
Nut <nuttasit.patarateerakul@gmail.com>
2016-11-15 17:55:55 +0700

    adding grid click event

--- src/app.js
+++ src/app.js
@@ -6,30 +6,42 @@
 		fullscreen : true
 	}).appendTo(elem);
 
-	// function createGrid(gap, radius){
-	// 	var grid = [];
-	// 	var gap = gap+radius
-	// 	var maxRow = Math.round(this.innerWidth / gap);
-	// 	var maxCol = Math.round(this.innerHeight / gap);
-	// 	var currentRow = 0;
-	// 	for (var j = 0; j < maxRow; j++) {
-	// 		var currentCol = 0;
-	// 		currentRow += gap;
-	// 		for (var i = 0; i < maxCol; i++) {
-	// 			currentCol += gap;
-	// 			var circle = two.makeCircle(currentRow, currentCol, radius);
-	// 			circle.linewidth = 0;
-	// 			circle.fill = "#888";
-	// 			grid.push(circle);
-	// 		};
-	// 	};
-	// 	return grid;
-	// }
+	function createGrid(gap, radius){
+		var grid = [];
+		var gap = gap+radius
+		var maxRow = Math.round(this.innerWidth / gap);
+		var maxCol = Math.round(this.innerHeight / gap);
+		var currentRow = 0;
 
-	for (var i = 0; i < 4; i++) {
-		var circle = two.makeCircle(10,10,10,10)
+		for (var j = 0; j < maxRow; j++) {
+			var currentCol = 0;
+			currentRow += gap;
+			for (var i = 0; i < maxCol; i++) {
+				currentCol += gap;
+				var circle = two.makeCircle(currentRow, currentCol, radius);
+				circle.linewidth = 0;
+				circle.fill = "#888";
+				circle.dom = document.querySelector('#' + circle.id);
+				grid.push(circle);
+			};
+		};
+		two.update();
+		return grid;
+	}
+
+	var grid = createGrid(150,10);
+
+	grid.addEvent = (function(){
+	for (var i = 0; i < this.length; i++) {
+		this[i].dom = document.querySelector('#' + this[i].id);
+		this[i].dom.addEventListener('click',onClick.bind(this[i]));
 	};
 
-	two.update();
+	function onClick(){
+		this.scale += 1
+		two.update();
+	}
+
+	}).bind(grid)();
 
 })();
